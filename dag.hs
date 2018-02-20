455b4522e0e800b33f24d03b8884ec1dc9d00020
Lorenz Gerber <lorenzottogerber@gmail.com>
2018-02-20 22:52:23 +0100

    started for real...

--- dag.hs
+++ dag.hs
@@ -1,50 +1,13 @@
-import Control.Monad.State
+import Data.List
 
-data Vertex = Vertex Id Weight [ Edge ] deriving (Show)
+
+data Vertex = Vertex Id Weight deriving (Show)
 data Edge = Edge Weight Vertex Vertex deriving (Show)
-data Dag = Dag ([ Vertex ], [ Edge ])   
+data Dag = Dag {vertices :: [ Vertex ], edges :: [ Edge ]} deriving (Show)   
 
 type Id = Int
 type Weight = Int
 
-
-data Person = Person {
-  id   :: Int,
-  name :: String
-} deriving Show
-
-startState = 0
-
-type PersonManagement = State Int
-
-generatePersonId :: PersonManagement Int
-generatePersonId = do
-    n <- get
-    put (n+1)
-    return n
-
-createPerson :: String -> PersonManagement Person
-createPerson name = do
-    id <- generatePersonId
-    return $ Person id name
-
-runPersonManagement :: PersonManagement a -> a
-runPersonManagement m = evalState m startState
-
-
-work :: PersonManagement (Person, Person)
-work = do
-    john <- createPerson "John"
-    steve <- createPerson "Steve"
-    return (john, steve)
-
-main = do
-    let (john, steve) = runPersonManagement work
-    putStrLn $ show john
-    putStrLn $ show steve
- 
-
-
-
---addVertex :: Dag -> Weight -> Dag
-
+addVertex :: Dag -> Weight -> Dag
+addVertex (Dag v w) weight = Dag (vertex:v) w
+    where vertex = Vertex ((length v)+97) weight
