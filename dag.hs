30124d002e39bba6f5fd03abb3c5b126d69cb560
Lorenz Gerber <lorenz.gerber@lablicate.com>
2018-02-20 18:08:42 +0100

    testing the state nomad example

--- dag.hs
+++ dag.hs
@@ -1,13 +1,50 @@
 import Control.Monad.State
-import System.Random
-import Data.UUID
 
 data Vertex = Vertex Id Weight [ Edge ] deriving (Show)
 data Edge = Edge Weight Vertex Vertex deriving (Show)
-data Dag =  Dage ([ Vertex ], [ Edge ])   
+data Dag = Dag ([ Vertex ], [ Edge ])   
 
 type Id = Int
 type Weight = Int
+
+
+data Person = Person {
+  id   :: Int,
+  name :: String
+} deriving Show
+
+startState = 0
+
+type PersonManagement = State Int
+
+generatePersonId :: PersonManagement Int
+generatePersonId = do
+    n <- get
+    put (n+1)
+    return n
+
+createPerson :: String -> PersonManagement Person
+createPerson name = do
+    id <- generatePersonId
+    return $ Person id name
+
+runPersonManagement :: PersonManagement a -> a
+runPersonManagement m = evalState m startState
+
+
+work :: PersonManagement (Person, Person)
+work = do
+    john <- createPerson "John"
+    steve <- createPerson "Steve"
+    return (john, steve)
+
+main = do
+    let (john, steve) = runPersonManagement work
+    putStrLn $ show john
+    putStrLn $ show steve
+ 
+
+
 
 --addVertex :: Dag -> Weight -> Dag
 
