381f84fb6b284c1cdb3520d984dcc1a2506e34b6
Lorenz Gerber <lorenz.gerber@lablicate.com>
2018-02-21 16:57:50 +0100

    algos

--- dag.hs
+++ dag.hs
@@ -1,13 +1,13 @@
 import Data.List
 
 
-data Vertex w = Vertex {vId :: Int
+data Vertex w = Vertex {vId :: Id
                        , vWeight :: Weight w
                        } deriving (Show)
 
 data Edge w = Edge { eWeight :: Weight w
-                   , origin :: Int
-                   , destination :: Int
+                   , origin :: Origin
+                   , destination :: Destination
                    } deriving (Show)
 
 data Dag v e = Dag { vertices :: [ Vertex v ]
@@ -19,8 +19,18 @@ data Weight a = Weight a deriving (Show, Eq)
 weightInt :: Int -> Weight Int
 weightInt a = Weight a
 
+type Id = Int
+type Origin = Int
+type Destination = Int
 
 addVertex :: Dag v e -> Weight v -> Dag v e 
 addVertex (Dag a b) c = Dag (vertex:a) b
-   where vertex = Vertex {vId = (length a), vWeight = c}  
- 
+    where vertex = Vertex {vId = (length a), vWeight = c}  
+
+addEdge :: Dag v e -> Origin -> Destination -> Weight e ->  Dag v e
+addEdge (Dag a b) c d e = Dag a (edge:b)
+    where edge = Edge {origin = c, destination = d, eWeight = e}
+
+
+
+
