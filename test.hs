dc5dad432962264af6c8f9396f6a5680494dfda1
Lorenz Gerber <lorenzottogerber@gmail.com>
2018-03-07 15:00:56 +0100

    fix on the type in test

--- test.hs
+++ test.hs
@@ -3,21 +3,21 @@ import Dag
 
  
 -- example data
-a = addVertex (Dag [][]) (Weight 'a') -- 0
-b = addVertex a (Weight 'b')          -- 1
-c = addVertex b (Weight 'c')          -- 2
-d = addVertex c (Weight 'd')          -- 3
-e = addVertex d (Weight 'e')          -- 4
-f = addVertex e (Weight 'f')          -- 5
-g = addVertex f (Weight 'g')          -- 6
-h = addEdge g 5 6 (Weight 'h')
-i = addEdge h 6 1 (Weight 'i')
-j = addEdge i 0 1 (Weight 'j')
-k = addEdge j 0 2 (Weight 'k')
-l = addEdge k 1 3 (Weight 'l')
-m = addEdge l 2 4 (Weight 'm')
-n = addEdge m 2 3 (Weight 'n')
-o = addEdge n 3 4 (Weight 'o')
+a = addVertex (Dag [][]) (Weight (1::Int)) -- 0
+b = addVertex a (Weight 2)          -- 1
+c = addVertex b (Weight 3)          -- 2
+d = addVertex c (Weight 4)          -- 3
+e = addVertex d (Weight 5)          -- 4
+f = addVertex e (Weight 6)          -- 5
+g = addVertex f (Weight 7)          -- 6
+h = addEdge g 5 6 (Weight 8)
+i = addEdge h 6 1 (Weight 9)
+j = addEdge i 0 1 (Weight 10)
+k = addEdge j 0 2 (Weight 11)
+l = addEdge k 1 3 (Weight 12)
+m = addEdge l 2 4 (Weight 13)
+n = addEdge m 2 3 (Weight 14)
+o = addEdge n 3 4 (Weight 15)
 
 p = weightLongestPath o 5 4 getWeightVertex getWeightEdge 
 
